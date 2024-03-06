9b51c15ba2c84367278d2b86562ea95861ce34b7
kwanda1999 <rosengwenya17@gmail.com>
2024-03-07 01:51:55 +0200

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movie4Free.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
